bbf78488cf9defeca60a8f8a20b9acb7b1233fe1
farahbousselmi <53794189+farahbousselmi@users.noreply.github.com>
2019-08-28 15:35:27 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MovieApp from "./MovieApp"
+import "./MovieApp.css";
+
+function App() {
+  return (
+    <div>
+      <MovieApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+export let moviesToDisplay = [
+  {
+    id: "movie1",
+    title: "Before sun rise",
+    rating: 5,
+    image: "http://www.gstatic.com/tv/thumb/v22vodart/16390/p16390_v_v8_aa.jpg",
+    year: 1995,
+
+  },
+  {
+    id: "movie2",
+    title: "Before sunset",
+    rating: 4,
+    image:
+      "http://fr.web.img4.acsta.net/medias/nmedia/18/35/22/35/18409539.jpg",
+    year: 2004,
+
+  },
+  {
+    id: "movie3",
+    title: "Before midnight",
+    rating: 3,
+    image:
+      "http://fr.web.img5.acsta.net/pictures/210/123/21012323_20130613143708915.jpg",
+    year: 2013,
+
+  }
+];
